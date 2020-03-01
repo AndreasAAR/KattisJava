21464e79dfdb49f63f6097388dfdf8ddd3a255e4
Andreas <Andreas.ahrlund@gmail.com>
2020-03-01 11:19:21 +0100

    Initial commit

---
+++ MatrixGameBinary.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class MatrixGameBinary {
+
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        int [][] input = matrixInput();
+    }
+
+    public static int[][] matrixInput(){
+
+
+
+        return null;
+    }
+
+}
+
+
+
+class Game{
+
+    private int[][] matrix = new int[4][4];
+
+    public Game(int[][] matrix){
+        this.matrix = matrix;
+    }
+
+
+  }
