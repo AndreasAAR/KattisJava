67e16bc2e775c374275a57535958f32119a87500
Andreas <Andreas.ahrlund@gmail.com>
2020-03-01 17:03:26 +0100

    Starting to break game up into classes

--- MatrixGameBinary.java
+++ MatrixGameBinary.java
@@ -1,31 +1,53 @@
 import java.util.Scanner;
 
 public class MatrixGameBinary {
-
+    Scanner scan;
+    int [][] input;
+    final int MATSIZE = 4;
     public static void main(String[] args){
-
-        Scanner scan = new Scanner(System.in);
-        int [][] input = matrixInput();
+        MatrixGameBinary mb = new MatrixGameBinary();
     }
 
-    public static int[][] matrixInput(){
+    MatrixGameBinary(){
+        scan = new Scanner(System.in);
+        input = matrixInput();
+        Game game = new Game(input);
+    }
 
 
+    public int[][] matrixInput(){
+        int[][] gameArr = new int [MATSIZE][MATSIZE];
 
-        return null;
+        for(int i = 0; i < MATSIZE ; i++){
+            String inputStr = scan.nextLine();
+            String[] numsStr = inputStr.split(" ");
+            for(int j = 0; j < MATSIZE ; j++){
+                gameArr[i][j] = Integer.parseInt(numsStr[j]);
+            }
+        }
+
+        return gameArr;
+    }
+
+
+    class Game{
+
+        private int[][] matrix = new int[MATSIZE][MATSIZE];
+
+        public Game(int[][] matrix){
+            this.matrix = matrix;
+            System.out.println(matrix);
+        }
+        public int[] flattenArray(int[] array){
+
+            return null;
+        }
+
+
     }
 
 }
 
 
 
-class Game{
 
-    private int[][] matrix = new int[4][4];
-
-    public Game(int[][] matrix){
-        this.matrix = matrix;
-    }
-
-
-  }
