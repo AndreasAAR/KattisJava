8f56322fb24c7c5b4a95ce828f74e78711be6a95
AndreasAAR <Andreas.ahrlund@gmail.com>
2020-05-01 18:35:33 +0200

    Finished Grand Adventure with stack

--- TheGrandAdventure.java
+++ TheGrandAdventure.java
@@ -1,27 +1,64 @@
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class TheGrandAdventure {
-    static private LinkedList<String> backpack;
-    static HashMap<String,String> enemyItem;
+    static private Stack<String> backpack = new Stack<>();
+    static HashMap<String,String> itemEnemy;
+    static final String GROUND = ".";
+    static Scanner scanner = new Scanner(System.in);
 
-    public static void loadList{
-       enemyItem.put("$","Money");
-       enemyItem.put("|","Incense");
-       
 
-                * represents Gem
+    public static void loadList(){
+       itemEnemy = new HashMap(){
+            {
+                put("|","t");
+                put("$","b");
+                put("*","j");
 
-        t represents a Trader
-
-        j represents a Jeweler
-
-        b represents a Banker
-
-. represents the Ground (nothing)
+            }
+        };
     }
 
     public static void main(String[] args){
          loadList();
+        int numAdventures = Integer.parseInt(scanner.nextLine());
+        testAdventures(numAdventures);
+    }
+
+    static public void testAdventures(int numAdvntrs){
+        for(int i = 0; i < numAdvntrs; i++){
+            String answer = (adventurePossible(scanner.nextLine())? "YES":"NO" );
+            System.out.println(answer);
+        }
+    }
+   static  public boolean adventurePossible(String adventure){
+       backpack.clear();
+       ArrayList<String> tiles = new ArrayList<>();
+       tiles.addAll( Arrays.asList(adventure.split("")));
+        for(int i = 0; i < tiles.size(); i++){
+            String currentTile = tiles.get(i);
+            String currentItem = itemEnemy.get(currentTile);
+
+            if(currentTile.equals(GROUND)){
+                continue;
+            }
+            //if we find an enemy
+            if(currentItem != null){
+               backpack.push(currentTile);
+            }else{ //Its an enemy!
+                if(backpack.isEmpty()){ //Empty backpack we lose
+                    return false;
+                }else{
+                   if(!itemEnemy.get(backpack.pop()).equals(currentTile)){
+                       return false;
+                   }
+                }
+            }
+        }
+        if(backpack.size() > 0){
+            return false;
+        }
+        return true;
     }
 }
+
--- Yoda.java
+++ Yoda.java
@@ -2,22 +2,71 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Yoda {
     public static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    public static int[] num1;
-    public static int[]  num2;
+    public static ArrayList<Integer> num1 = new ArrayList<>();
+    public static ArrayList<Integer>  num2 = new ArrayList<>();
     public static void main(String[] args){
          makeNumArrs();
     }
     public static void makeNumArrs(){
        fillNumArr(num1);
        fillNumArr(num2);
+       collideNums();
     }
 
-    private static void fillNumArr(int[] nums){
+    private static void collideNums(){
+        ArrayList<Integer> orgNum1 = (ArrayList<Integer>) num1.clone();
+        ArrayList<Integer> orgNum2 = (ArrayList<Integer>) num2.clone();
+        if(isYoda(num1,num2)){
+            System.out.println("YODA");
+            System.out.println(makeNumStr(num2));
+            return;
+        }
+        throwOut(num1, orgNum2);
+        throwOut(num2, orgNum1);
+    }
+
+    private static String makeNumStr(ArrayList<Integer> num){
+        String strNum = "";
+
+        for(Integer in: num){
+            strNum += in;
+        }
+
+        return strNum;
+    }
+
+    private static boolean isYoda(ArrayList<Integer> smallNum,ArrayList<Integer> bigNum){
+
+        for(Integer s: smallNum){
+            for(Integer b: bigNum){
+                if(s > b)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static void throwOut(ArrayList<Integer> targNum,ArrayList<Integer> apNum){
+        int shortLen = (targNum.size() < apNum.size())? targNum.size() : apNum.size();
+
+        for(int i = shortLen-1; i > 0; i--){
+            
+        }
+    }
+
+    private static void fillNumArr(ArrayList<Integer> nums){
        String[] numStrs =  takeInput();
 
+       for(String s : numStrs){
+           nums.add(Integer.parseInt(s));
+       }
+       return;
     }
 
     private static String [] takeInput(){
@@ -30,6 +79,4 @@ public class Yoda {
         }
         return null;
     }
-
-
 }
